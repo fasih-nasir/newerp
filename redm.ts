9d2d901e5e52566f626efbadde9ec709b5febcf9
fndeveloper <fasihnasir60@gmail.com>
2025-04-27 19:40:33 +0500

    code is here

--- redm.ts
+++ redm.ts
@@ -4,7 +4,19 @@ fetch("https://opensheet.elk.sh/apihere/Sheet1")
 // <button
 // class="mt-3 btn3 col-lg-2 col-6 p-2 d-flex flex-row align-items-center justify-content-center ">
 // Get Started
+// <a href="Apply_Now.html">
 // <i class="fa-solid fa-paper-plane icon-hover col-lg-12">
 
 // </i>
+// </a>
 // </button>
+
+
+
+
+
+// <div class="mb-3 d-flex align-items-center gap-2">
+//                 <div class="bg-col rounded-circle" style="width:10px; height:10px;"></div>
+//                 <h5 class="col-org mb-0">About</h5>
+//               </div>
+              
